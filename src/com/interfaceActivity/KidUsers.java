d6c69cb90b580d190ff144cb48116235003e943d
Debajyoti Bhattacharjee <debajyoti0007@gmail.com>
2022-01-26 21:54:51 +0530

    test

---
+++ src/com/interfaceActivity/KidUsers.java
@@ -0,0 +1,35 @@
+package com.interfaceActivity;
+
+class KidUsers implements LIbraryUser {
+	
+	int age;
+	String bookType;
+	
+
+
+	@Override
+	public void registerAccount() {
+		// TODO Auto-generated method stub
+		if(age<12) {
+			System.out.println("You have successfully registered under a Kids Account");
+		}
+		else {
+			System.out.println("Sorry, Age must be less than 12 to register as a kid");
+		}
+
+	}
+
+	@Override
+	public void requestBook() {
+		// TODO Auto-generated method stub
+		if(bookType=="Kids") {
+			System.out.println("Book Issued successfully, please return the book within 10 days");
+		}
+		else {
+			System.out.println("Oops, you are allowed to take only kids books");
+		}
+		
+
+	}
+
+}
